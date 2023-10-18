96a01fba40a289d3437d1aabf73682e1b10a4217
add-itya <jitheshaditya@gmail.com>
2023-10-18 13:51:59 -0400

    temp

--- tof-website/src/App.js
+++ tof-website/src/App.js
@@ -1,11 +1,34 @@
-import "./App.css";
-import Webcam from "react-webcam";
-
+import React, { useRef, useState } from 'react';
+import Webcam from 'react-webcam';
 
 function App() {
+  const webcamRef = useRef(null);
+  const [framesList, setFramesList] = useState([]);
+  
+
+  const captureFrame = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setFramesList((prevFramesList) => [...prevFramesList, imageSrc]);
+  };
+
   return (
-    <div className="App">
-      <Webcam />
+    <div>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+      />
+      <button onClick={captureFrame}>Capture Frame</button>
+      <div>
+        {framesList.map((frame, index) => (
+          <img
+            key={index}
+            src={frame}
+            alt={`Frame ${index}`}
+            width="320"  // You can adjust the width as needed
+          />
+        ))}
+      </div>
     </div>
   );
 }
