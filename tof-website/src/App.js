8369d6e26a5edb15583dd43b3e282f4f614f4dd4
add-itya <jitheshaditya@gmail.com>
2023-11-02 19:11:32 -0400

    recording feature sending to backend

--- tof-website/src/App.js
+++ tof-website/src/App.js
@@ -7,8 +7,11 @@ function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
   const detectorRef = useRef(null);
-  const [handLandmarks, setHandLandmarks] = useState(null);
+  
   tf.setBackend('webgl'); // You can also try 'cpu' or 'wasm' as alternatives
+
+  let isRecording = false;
+  const recordedData = [];
 
   // Load the hand-pose detection model when the component mounts
   useEffect(() => {
@@ -25,44 +28,62 @@ function App() {
     loadHandPoseModel();
   }, []);
 
-  // Function to capture and process the webcam frame
   const captureAndProcessFrame = async () => {
     if (detectorRef.current && webcamRef.current && canvasRef.current) {
       const imageBase64 = webcamRef.current.getScreenshot();
-
       // Create an HTMLImageElement and set its src to the base64 image
       const img = new Image();
       img.src = imageBase64;
 
       img.onload = async () => {
         const hands = await detectorRef.current.estimateHands(img);
-        console.log(hands)
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext('2d');
-        ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
 
-        if (hands.length !== 0) {
-          ctx.fillStyle = hands[0].handedness === 'Left' ? 'orange' : 'blue';
-          ctx.strokeStyle = 'white';
-          ctx.lineWidth = 2;
-
-          for (let y = 0; y < hands[0].keypoints.length; y++) {
-            const keypoint = hands[0].keypoints[y];
-            ctx.beginPath();
-            ctx.arc(keypoint.x, keypoint.y, 4, 0, 2 * Math.PI);
-            ctx.fill();
-          }
+        const keypointsData = [];
+        if (hands.length !== 0 && (isRecording)) {
+          console.log("here")
+          keypointsData.push(
+            hands[0].keypoints3D[4].x,
+            hands[0].keypoints3D[4].y,
+            hands[0].keypoints3D[4].z,
+            hands[0].keypoints3D[20].x,
+            hands[0].keypoints3D[20].y,
+            hands[0].keypoints3D[20].z,
+          );
+          recordedData.push(keypointsData);
+        } else if (isRecording) {
+          alert("No hand detected")
+          stopRecording();
         }
       };
     }
-    requestAnimationFrame(captureAndProcessFrame); // Continuously request frames
+    requestAnimationFrame(captureAndProcessFrame);
   };
 
-  // Use requestAnimationFrame to continuously run the captureAndProcessFrame function
+  const startRecording = () => {
+    isRecording = true;
+  };
+  
+
+  const stopRecording = () => {
+    isRecording = false;
+    let data = {
+      "landmarks": recordedData
+    }
+    console.log(JSON.stringify(data))
+    fetch('http://35.172.96.103:3001/calculate-ratio', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    })
+    // Clear the recorded data to allow the user to record again
+    recordedData.length = 0;
+  };
+
   useEffect(() => {
     requestAnimationFrame(captureAndProcessFrame);
 
-    // Clean up by stopping the detector when the component unmounts
     return () => {
       if (detectorRef.current) {
         detectorRef.current.close();
@@ -75,9 +96,11 @@ function App() {
       <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
       <canvas
         ref={canvasRef}
-        width={640} // Adjust the canvas width to match your webcam resolution
-        height={480} // Adjust the canvas height to match your webcam resolution
+        width={640}
+        height={480}
       />
+      <button onClick={startRecording}>Start Recording</button>
+      <button onClick={stopRecording}>Stop Recording</button>
     </div>
   );
 }
